24a2d84ecb7aa4090b79f40d0d681246d3c51f98
unknown <quvp861205@gmail.com>
2021-05-31 19:43:15 -0600

    Primera version del proyecto: actividad principal y fragmento home

---
+++ PlatziConf/app/src/main/java/com/example/platziconf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.platziconf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.platziconf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
