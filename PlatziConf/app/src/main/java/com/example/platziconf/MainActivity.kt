b27d7c214322311a4f9d25e8226affa43498dee0
unknown <quvp861205@gmail.com>
2021-06-23 15:43:54 -0600

    se finaliza el diseño de todas las ventanas de mockup

--- PlatziConf/app/src/main/java/com/example/platziconf/MainActivity.kt
+++ PlatziConf/app/src/main/java/com/example/platziconf/MainActivity.kt
@@ -6,19 +6,34 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
 
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+
+        menuInflater.inflate(R.menu.bnv_menu, menu)
         return true
+    }
+
+    private fun configNav(){
+        /*val navController = findNavController(R.id.nav_graph)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)*/
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -26,8 +41,9 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.bnvMenu -> true
             else -> super.onOptionsItemSelected(item)
         }
+
     }
 }
